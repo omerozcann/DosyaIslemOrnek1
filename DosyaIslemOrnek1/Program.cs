3eae480ab2c90039598f38b05bc6813935af012d
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2023-12-29 10:44:10 +0300

    Proje dosyası ekle.

---
+++ DosyaIslemOrnek1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DosyaIslemOrnek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcıdan alınan Ders adı, sınav türü ve sınav notunu 
+            //dosyaya kaydedecek programı yazınız.
+            String dersadi, sinavturu, sinavnotu;
+            Console.WriteLine("Ders Adı:");
+            dersadi= Console.ReadLine();
+            Console.WriteLine("Sınav Türü:");
+            sinavturu = Console.ReadLine();
+            Console.WriteLine("Sınav Notu:");
+            sinavnotu = Console.ReadLine();
+            File.AppendAllText("d:\\notlarim.txt", dersadi + "-" + sinavturu + "-" + sinavnotu+"\n");
+            //satır sonundaki \n bir alt satıra inmek için kullanılır.
+        }
+    }
+}
